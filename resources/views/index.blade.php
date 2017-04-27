f594aba4cc7ea6e65a789ec63621031e9c4b777e
Shady <e.saw.90@gmail.com>
2017-04-27 12:10:41 +0200

    add changed home

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,83 +15,8 @@
 								<a href="#"><img src="img/blog-1.jpg" alt="" class="img-responsive"></a>
 								<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor dunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justoduo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dol sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmodtempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. </p>
 								<p>At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no takimata sanctus est Loripsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitsed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed dit luptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
-								<div class="read-more text-center">
-									<a href="#">Read the article</a>
-								</div>
 							</div>
 						</article>
-
-						<article class="single-post">
-							<div class="post-header text-center">
-								<h2><a href="#">a lovely evening with my beloved</a></h2>
-								<span>Written by <a href="#">John Doe</a> in <a href="#">Development</a> on October 20, 2015 </span>
-							</div>
-							<div class="post-content">
-								<a href="#"><img src="img/blog-2.jpg" alt="" class="img-responsive"></a>
-								<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor dunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justoduo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dol sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmodtempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. </p>
-								<p>At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no takimata sanctus est Loripsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitsed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed dit luptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
-								<div class="read-more text-center">
-									<a href="#">Read the article</a>
-								</div>
-							</div>
-						</article>
-
-						<article class="single-post">
-							<div class="post-header text-center">
-								<h2><a href="#">Snacks after hard working</a></h2>
-								<span>Written by <a href="#">John Doe</a> in <a href="#">Design</a> on October 23, 2015 </span>
-							</div>
-							<div class="post-content">
-								<a href="#"><img src="img/blog-3.jpg" alt="" class="img-responsive"></a>
-								<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor dunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justoduo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dol sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmodtempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. </p>
-								<p>At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no takimata sanctus est Loripsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitsed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed dit luptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
-								<div class="read-more text-center">
-									<a href="#">Read the article</a>
-								</div>
-							</div>
-						</article>
-
-						<article class="single-post">
-							<div class="post-header text-center">
-								<h2><a href="#">Had a wonderful dinner</a></h2>
-								<span>Written by <a href="#">John Doe</a> in <a href="#">Development</a> on October 25, 2015 </span>
-							</div>
-							<div class="post-content">
-								<a href="#"><img src="img/blog-4.jpg" alt="" class="img-responsive"></a>
-								<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor dunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justoduo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dol sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmodtempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. </p>
-								<p>At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no takimata sanctus est Loripsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitsed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed dit luptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
-								<div class="read-more text-center">
-									<a href="#">Read the article</a>
-								</div>
-							</div>
-						</article>
-
-						<article class="single-post">
-							<div class="post-header text-center">
-								<h2><a href="#">Wonderful site view in malaysia</a></h2>
-								<span>Written by <a href="#">John Doe</a> in <a href="#">Travel</a> on October 29, 2015 </span>
-							</div>
-							<div class="post-content">
-								<a href="#"><img src="img/blog-5.jpg" alt="" class="img-responsive"></a>
-								<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor dunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justoduo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dol sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmodtempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. </p>
-								<p>At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no takimata sanctus est Loripsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitsed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed dit luptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
-								<div class="read-more text-center">
-									<a href="#">Read the article</a>
-								</div>
-							</div>
-						</article>
-						<div class="post-pagination">
-							<div class="col-md-6 col-sm-6 col-xs-6">
-								<ul class="list-inline">
-									<li class="current"><a href="#">1</a></li>
-									<li><a href="#">2</a></li>
-									<li><a href="#">3</a></li>
-								</ul>
-							</div>
-							<div class="col-md-6 col-sm-6 col-xs-6 text-right">
-								<a href="#">Older Articles <i class="fa fa-angle-double-right"></i></a>
-							</div>
-						</div>
 					</div>
 				</div>
 			</div>
