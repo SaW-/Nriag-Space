cb1dabd336051abd013f8954f4c7c8a64f9a08c1
Shady <e.saw.90@gmail.com>
2017-04-30 11:11:58 +0200

    about us

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -6,12 +6,11 @@
 			<div class="row">
 				<div class="col-md-8 col-md-offset-2">
 					<h2>About Team</h2>
-					<!--<span>UI/UX Designer &amp; Developer</span>
+					<!--<span>UI/UX Designer &amp; Developer</span>-->
 
-					<img src="img/kallu.jpg" alt="" class="img-responsive">-->
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, veritatis nulla in animi dolorem cum ipsum repellendus perferendis fugiat totam vel, aut laborum sit inventore, eum consequatur voluptatem rerum itaque velit! Est doloremque maiores assumenda commodi similique eum ex eligendi, voluptates omnis earum necessitatibus sunt velit, eius laboriosam. Dignissimos ex velit quidem repudiandae, ut laudantium optio, totam reprehenderit illum dolorum? Iure ipsam obcaecati omnis rem, quibusdam fugiat voluptates quidem officia odio nobis ad sed ab nemo nesciunt, quos asperiores? Aspernatur nulla consectetur sit ducimus quia quod eius, veniam doloremque minima quis dolores quasi officiis est harum, totam maxime quas. Voluptates.</p>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci officia eos laboriosam similique dolorum, fugit a debitis, optio impedit recusandae natus omnis blanditiis veniam ad ipsam nulla eius facere, dolores! Aut, eligendi, velit quidem omnis debitis illo ratione atque dolore alias voluptate temporibus, dignissimos, tenetur minima ipsa delectus doloribus? Vel?</p>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae possimus recusandae explicabo veniam, ratione. Adipisci harum, eaque, possimus corporis architecto minus dolorem ipsa aliquid magni deleniti laudantium neque facilis excepturi culpa exercitationem maiores? Veniam dolorem dolores magni doloremque atque ad, beatae accusamus quas obcaecati quia, accusantium harum expedita numquam corporis voluptatem inventore soluta. Nemo corporis adipisci minima soluta laborum eligendi laboriosam omnis facilis inventore, recusandae possimus libero perspiciatis placeat quisquam!</p>
+					<img src="img/team.jpg" alt="" class="img-responsive">
+					<p>Hello, We are NRIAG Space.</p>
+					<p>Our Vision to create an interactive 3D web application that help everyone especially students and teachers know application of space science in earth study through showing the earth science satellites and it’s trajectories ,instruments ,earth coverage and output data visualization. This application is a time and search effort saver.</p>
 				</div>
 			</div>
 		</div>
